62815ae132479090093dcf2423911ccc89f59509
Jeff Cragle <jeffcragle@protonmail.com>
2020-07-26 08:47:36 -0400

    refactored calculator.js

--- lesson_2/calculator.js
+++ lesson_2/calculator.js
@@ -6,37 +6,56 @@
 
 const readline = require('readline-sync');
 
-console.log('Welcome to Calculator!');
+function prompt(message) {
+  console.log(`=> ${message}`);
+}
 
-console.log("What's the first number?");
+function invalidNumber(number) {
+  return number.trimStart() === '' || Number.isNaN(Number(number));
+}
+
+prompt('Welcome to Calculator!');
+
+prompt("What's the first number?");
 let number1 = readline.question();
 
-console.log("What's the second number?");
+while (invalidNumber(number1)) {
+  prompt("That doesn't look like a valid number, try again. ");
+  number1 = readline.question();
+}
+
+prompt("What's the second number?");
 let number2 = readline.question();
 
-console.log(
+while (invalidNumber(number2)) {
+  prompt("That doesn't look like a valid number, try again. ");
+  number2 = readline.question();
+}
+
+prompt(
   'What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide?'
 );
 let operation = readline.question();
 
 let output;
-if (operation === '1') {
-  // '1' represents additions
-  output = Number(number1) + Number(number2);
-} else if (operation === '2') {
-  // '2' represents subtraction
-  output = Number(number1) - Number(number2);
-} else if (operation === '3') {
-  // '3' represents multiplication
-  output = Number(number1) * Number(number2);
-} else if (operation === '4') {
-  // '4' represents division
-  if (number2 !== '0') {
+switch (operation) {
+  case '1':
+    output = Number(number1) + Number(number2);
+    break;
+  case '2':
+    output = Number(number1) - Number(number2);
+    break;
+  case '3':
+    output = Number(number1) * Number(number2);
+    break;
+  case '4':
     output = Number(number1) / Number(number2);
-  } else {
-    number2 = readline.question('input a non-zero number! ');
-  }
-  output = Number(number1) / Number(number2);
+    break;
 }
 
-console.log(`The result is: ${output}`);
+while (!['1', '2', '3', '4'].includes(operation)) {
+  prompt('Please choose an operation!');
+  operation = readline.question();
+}
+
+prompt(`The result is: ${output}`);
