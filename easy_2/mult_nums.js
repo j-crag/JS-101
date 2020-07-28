bfc89634bae2a2554d813d1cf5ffd94a4aff98a2
Jeff Cragle <jeffcragle@protonmail.com>
2020-07-27 20:06:24 -0400

    updated folder structure

--- easy_2/mult_nums.js
+++
@@ -1,9 +0,0 @@
-function multiply(num1, num2) {
-  return num1 * num2;
-}
-
-console.log(multiply(5, 5) === 25);
-
-const multiply2 = (num1, num2) => num1 * num2;
-
-console.log(multiply2(5, 5) === 25);
