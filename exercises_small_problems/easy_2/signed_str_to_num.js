45dbe93607f5e92a9718565483024e07cc28a8ae
Jeff Cragle <jeffcragle@protonmail.com>
2020-08-14 10:06:45 -0400

    solutions easy 2

--- exercises_small_problems/easy_2/signed_str_to_num.js
+++ exercises_small_problems/easy_2/signed_str_to_num.js
@@ -1,37 +1,46 @@
-function toInteger(string) {
-  const DIGITS = {
-    1: 1,
-    2: 2,
-    3: 3,
-    4: 4,
-    5: 5,
-    6: 6,
-    7: 7,
-    8: 8,
-    9: 9,
-    0: 0,
-  };
+// explicit rules:
+//  - convert non negative integer to string representation of integer.
+//  - no conversion methods
 
-  let arrayOfDigits = string.split('').map((char) => DIGITS[char]);
-  let value = 0;
-  arrayOfDigits.forEach((digit) => (value = (10 * value) + digit));
-  return value;
+// Algorithm:
+//  - create object charNums with number to string pairings from 0 to 9.
+//  - create empty string to contain string representations of numbers.
+//  - take the last digit of the current number and set it to variable "lastDigit"
+//    - do this by taking the remainder of the number (number % 10 = lastDigit)
+//  - remove last digit by dividing number by 10 and reassigning to number variable
+//  - use last digit to look up string representation in object charNums
+//  - add this character to the results string
+//  - repeat while the number is > 0
+
+function integerToString(number) {
+  const CHAR_NUMS = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  let result = '';
+
+  do {
+    let lastDigit = number % 10;
+    number = (number - lastDigit) / 10;
+    result = CHAR_NUMS[lastDigit] + result;
+  } while (number > 0);
+
+  return result;
 }
 
-function signedInteger(string) {
-  switch (string[0]) {
-    case '-':
-      return -toInteger(string.slice(1, string.length));
-      break;
-    case '+':
-      return toInteger(string.slice(1, string.length));
-      break;
+//examples
+
+console.log(integerToString(123)); // => '123'
+console.log(integerToString(1)); // => '1'
+
+function signedIntegerToString(number) {
+  let negativeResult = '-';
+  let positiveResult = '+';
+  switch (Math.sign(number)) {
+    case -1:
+      return `-${integerToString(-number)}`;
     default:
-      return toInteger(string);
+      return integerToString(number);
   }
 }
 
-console.log(signedInteger('-123'));
-console.log(signedInteger('123'));
-console.log(signedInteger('+123'));
-
+console.log(signedIntegerToString(-100)); // => '-100'
+console.log(signedIntegerToString(+100)); // => '100'
+console.log(signedIntegerToString(100)); // => '100'
